ff5009f5a58b6577d48655b80aca80ee0c2c27f6
st46593 <st46593@student.upce.cz>
2020-09-07 22:58:31 +0200

    second request?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <AnchorLink class="navbar-brand brand-size" href="#services">JMK služby s. r. o. VOJTĚCHOV</AnchorLink>
+      <AnchorLink class="navbar-brand brand-size" href="#services"><div class="mx-2 row font-weight-bold">JMK SLUŽBY S. R. O.</div><div class="mx-2 row h6">VOJTĚCHOV</div></AnchorLink>
       <button class="navbar-toggler toggler-size" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,12 +13,12 @@ function Navbar() {
         <ul class="nav navbar-nav ml-auto">
           <li class="nav-item">
             <AnchorLink class="nav-link" href="#services">
-              Nabízíme
+              NABÍDKA
               </AnchorLink>
           </li>
           <li class="nav-item">
             <AnchorLink class="nav-link" href="#contact">
-              Kontakt
+              KONTAKT
             </AnchorLink>
           </li>
         </ul>
--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -7,7 +7,7 @@ function ServiceItem({ icon, title, text }) {
     <Fade bottom>
       <div className="col-lg-5 col-sm-12 service-item">
         <FontAwesomeIcon className="service-icon" size="3x" icon={icon} />
-        <h4 className="mt-3 mx-2 text-left" style={{fontFamily: "Raleway, Helvetica, Arial, sans-serif", fontWeight: "600"}}>{title}</h4>
+        <h6 className="mt-3 mx-2 text-left" style={{fontFamily: "Raleway, Helvetica, Arial, sans-serif", fontWeight: "600"}}>{title}</h6>
         <p>
      {text.map(item=><li class="service-text">{item}</li>)}
         </p>
