3d4d8e73632de3b4f0d32ff13140445bbfdd5a5f
st46593 <st46593@student.upce.cz>
2021-01-28 21:24:24 +0100

    cms fixing

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import CMS from 'netlify-cms-app'
-import ServicePageTemplate from '../subpages/ServicePageTemplate'
+import ServicePageTemplate from '../subpages/ServicesPage'
 
 CMS.registerPreviewTemplate('services-page', ({ entry }) => (
     <ServicePageTemplate {...entry.toJS().data} />
